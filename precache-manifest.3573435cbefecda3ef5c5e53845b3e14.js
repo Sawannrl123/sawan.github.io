dcc0aec06ae3bda216898b88404b5031fbb4b067
Sawan Kumar <sawankumar@Sawans-MacBook-Pro.local>
2020-02-11 18:18:01 +0530

    Updates

---
+++ precache-manifest.3573435cbefecda3ef5c5e53845b3e14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9871ed43ddac303e99bda2b1452da4e4",
+    "url": "/sawan/index.html"
+  },
+  {
+    "revision": "07315923f9e1ca270af5",
+    "url": "/sawan/static/css/main.0298122a.chunk.css"
+  },
+  {
+    "revision": "3936bac4059ee08eb335",
+    "url": "/sawan/static/js/2.92951342.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sawan/static/js/2.92951342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07315923f9e1ca270af5",
+    "url": "/sawan/static/js/main.a368e804.chunk.js"
+  },
+  {
+    "revision": "bfb1632fac5114d3b46c",
+    "url": "/sawan/static/js/runtime-main.8572390e.js"
+  },
+  {
+    "revision": "be82276550361f1f7cb53cd53c14cfb7",
+    "url": "/sawan/static/media/logo.be822765.png"
+  }
+]);

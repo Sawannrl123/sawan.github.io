a1ea0b1b123307865008eb1fccc839ce32124133
Sawan Kumar <sawan.kumar@idc.bedbath.com>
2021-04-10 10:41:57 +0530

    Updates

---
+++ precache-manifest.e53c5566ca78356e3007072809844718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e9aad0ee3a50b2b99032872ced8bc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc36909611d742a65001",
+    "url": "/static/js/2.4c2816ed.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4c2816ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d661b4052c885dcd4d3f",
+    "url": "/static/js/main.a7c3bdd0.chunk.js"
+  },
+  {
+    "revision": "26a20049dfa9a61893eb",
+    "url": "/static/js/runtime-main.d0f1732f.js"
+  }
+]);
